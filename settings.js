5632c3998a9bf1c3035085a7a128888ced2b160a
Andanjox <114666387+Andanjox@users.noreply.github.com>
2022-09-30 23:25:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.owner = ['+919728714060']
+global.premium = ['+919728714060']
+global.ownernomer = '+919728714060'
+global.ownername = 'SANJU'
+global.botname = 'SANJU-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
@@ -31,7 +31,7 @@ global.sc = 'https://github.com/darkalphaxteam/QUEEN-BIXBY-MD-PRO-NEW'
 global.myweb = 'https://youtube.com/channel/UCvpyOCBCaBSCdv4QolnO0WQ'
 global.packname = 'Queen Bixby MD'
 global.author = 'DARK ALPHA'
-global.sessionName = 'session'
+global.sessionName = 'SANJU'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
 global.mess = {
